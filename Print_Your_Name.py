1284492ca45c33bfa24c0f3ccf6130744d0eb55a
Payal-Kumari-2302 <jaiswalpayal0403@gmail.com>
2026-05-06 22:19:19 +0530

    Update Print_Your_Name.py

--- Print_Your_Name.py
+++ Print_Your_Name.py
@@ -1,8 +1,10 @@
 #Program_2
 
+# Program 2: Printing a Name
+
+# Method 1: Direct printing
 print("Payal")
 
-            Method_2
-
-name="Payal"
+# Method 2: Using a variable
+name = "Payal"  # Store the name in a variable
 print(name)
