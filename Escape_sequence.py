e6906e69cbcaabb81c2c6a748aee52ad2d0b865d
Payal-Kumari-2302 <jaiswalpayal0403@gmail.com>
2026-05-06 22:34:59 +0530

    Create Escape_sequence.py

---
+++ Escape_sequence.py
@@ -0,0 +1,28 @@
+# Program_3
+# W.A Python Program to print all escape sequences
+
+# A. New Line (\n)
+print("Payal\nKumari")
+
+# B. Tab Space (\t)
+print("Payal\tJaiswal")
+
+# C. Vertical Tab (\v)
+print("Payal\vJaiswal")
+
+# D. Backspace (\b)
+print("Ram is a box\by")
+
+# E. Backslash (\\)
+print("Payal\\Babli")
+
+# F. Double Quote (\")
+print("\"Payal\"")
+
+# G. Single Quote (\')
+print("Name\'s")
+
+# H. Carriage Return (\r)
+print("Sohan is a boy\rM")
+
+
