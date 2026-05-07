5a4b04f88444471d8300a0e537d81ea56dd18a01
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-07 15:36:32 +0530

    Create Variable_Program.py
    A collection of beginner-friendly Python programs based on variables and data types. These programs cover variable declaration, multiple assignments, user input handling, type checking, and constant variables.

---
+++ Variable_Program.py
@@ -0,0 +1,51 @@
+# Program_6
+
+#Variable Related Program
+
+#1st_program
+#Variable Declaration
+
+name="Payal"
+age=22
+marks=9.18
+
+print(name)
+print(age)
+print(marks)
+
+#2nd_Program
+#Multiple variable Assignment(Multiple Variable in one line)
+
+a,b,c=10,20,30
+
+print(a)
+print(b)
+print(c)
+
+#3rd_Program
+#Accept User Input using variable
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+
+print("Name =", name)
+print("Age =", age)
+
+#4th_Program
+# Check data type of variables
+
+a = 10
+b = 5.5
+c = "Python"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+#5th_Program
+# Constant variable example
+
+PI = 3.14
+
+print("Value of PI =", PI)
