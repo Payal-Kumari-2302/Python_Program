6897ec68ba86aa2f73462170c63d050aad6db303
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-07 15:16:00 +0530

    Create Additional_Mathematical_Operation.py

---
+++ Additional_Mathematical_Operation.py
@@ -0,0 +1,17 @@
+# Program_5
+
+# W.A.P to perform additional mathematical operations
+
+a, b = 5, 3
+
+c = a**2 + b**2
+d = a**2 - b**2
+e = a**2 * b**2
+f = a**2 / b**2      # or a**2 // b**2
+g = a**2 % b**2
+
+print("Addition of squares =", c)
+print("Subtraction of squares =", d)
+print("Multiplication of squares =", e)
+print("Division of squares =", f)
+print("Remainder =", g)
