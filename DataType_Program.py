1fd680c1b4a4739ff7662b62d4a44e1d651a3e5d
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-08 14:09:30 +0530

    Create DataType_Program.py
    A beginner-friendly Python program that explains various built-in data types with practical examples and output demonstration.

---
+++ DataType_Program.py
@@ -0,0 +1,50 @@
+#Program : Python Data Types
+
+# Integer Data Type
+num = 10
+print("Integer Value =", num)
+print("Data Type =", type(num))
+
+# Float Data Type
+price = 99.5
+print("\nFloat Value =", price)
+print("Data Type =", type(price))
+
+# Complex Data Type
+complex_num = 2 + 3j
+print("\nComplex Value =", complex_num)
+print("Data Type =", type(complex_num))
+
+# String Data Type
+name = "Payal"
+print("\nString Value =", name)
+print("Data Type =", type(name))
+
+# Boolean Data Type
+result = True
+print("\nBoolean Value =", result)
+print("Data Type =", type(result))
+
+# List Data Type
+numbers = [1, 2, 3, 4]
+print("\nList Value =", numbers)
+print("Data Type =", type(numbers))
+
+# Tuple Data Type
+data = (10, 20, 30)
+print("\nTuple Value =", data)
+print("Data Type =", type(data))
+
+# Set Data Type
+items = {1, 2, 3, 4}
+print("\nSet Value =", items)
+print("Data Type =", type(items))
+
+# Dictionary Data Type
+student = {
+    "name": "Payal",
+    "age": 22
+}
+print("\nDictionary Value =", student)
+print("Data Type =", type(student))
+
