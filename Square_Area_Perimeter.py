37a1513dbed355334eaa9a04ef96122ddfe3dbee
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-08 18:15:32 +0530

    Create Square_Area_Perimeter.py
    Python program to calculate the area and perimeter of a square using side input.

---
+++ Square_Area_Perimeter.py
@@ -0,0 +1,12 @@
+# Program_10
+
+# W.A.Python program to accept the side of a square
+# and find the area and perimeter of the square.
+
+side = float(input("Enter side of square in cm:-> "))
+
+area = side * side
+perimeter = 4 * side
+
+print("Area of square:->", area)
+print("Perimeter of square:->", perimeter)
