22cf2d74f36dad8fee7b3c06208dbb93996fa713
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-09 19:18:36 +0530

    Create Person_Details_And_TestScore.py
    Python program to store and display a person's name, age, and average test score.

---
+++ Person_Details_And_TestScore.py
@@ -0,0 +1,11 @@
+# Program
+
+# Create variables for storing a person's name, age, and average test score.
+
+name = input("Enter your name:-> ")
+age = int(input("Enter your age:-> "))
+average_test_score = 87.20
+
+print("Your name is", name)
+print("Your age is", age)
+print("Average test score is", average_test_score)
