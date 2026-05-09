ac474ba367280cad25531d7ca18d8393b85f2f08
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-09 19:07:25 +0530

    Create Area_And_Perimeter_Of_Triangle.py
    Python program to calculate the area and perimeter of a triangle using Heron’s Formula.

---
+++ Area_And_Perimeter_Of_Triangle.py
@@ -0,0 +1,20 @@
+# Program_12
+
+# W.A.Python program to accept three sides of a triangle and calculate the area, perimeter and semi perimeter of the triangle.
+
+import math
+
+s1 = int(input("Enter 1st side of triangle:-> "))
+s2 = int(input("Enter 2nd side of triangle:-> "))
+s3 = int(input("Enter 3rd side of triangle:-> "))
+
+# Semi-perimeter
+s = (s1 + s2 + s3) / 2
+
+# Heron's Formula
+area = math.sqrt(s * (s - s1) * (s - s2) * (s - s3))
+
+perimeter = s1 + s2 + s3
+
+print("Area of triangle:->", area)
+print("Perimeter of triangle:->", perimeter)
