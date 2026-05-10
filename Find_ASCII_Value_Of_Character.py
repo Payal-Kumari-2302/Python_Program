ce31aeb250570ce561c0bcdc989ae26c4e1d7455
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-10 12:52:09 +0530

    Create Find_ASCII_Value_Of_Character.py
    Python program to accept a character from the user and display its ASCII value using ord() function.

---
+++ Find_ASCII_Value_Of_Character.py
@@ -0,0 +1,10 @@
+#Program_19
+
+# W.A.Python program to accept a character
+# and display its ASCII value.
+
+ch = input("Enter any character:-> ")
+
+ascii_value = ord(ch)
+
+print("ASCII value is:->", ascii_value)
