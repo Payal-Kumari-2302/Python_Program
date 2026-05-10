ffeccea23f4cea1ff965f8b98c37489a369b0d6d
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-10 12:48:06 +0530

    Create Convert_Minutes_To_Hours_And_Minutes.py
    Python program to convert total minutes into hours and remaining minutes using arithmetic operators.

---
+++ Convert_Minutes_To_Hours_And_Minutes.py
@@ -0,0 +1,11 @@
+#Program_16
+
+# W.A.Python program to convert minutes into hours and minutes.
+
+total_min = int(input("Enter number of minutes:-> "))
+
+hours = total_min // 60
+minutes = total_min % 60
+
+print("Hours:->", hours)
+print("Minutes:->", minutes)
