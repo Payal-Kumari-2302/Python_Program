9a53090346c483dc1492910d98d0e89b08caeeed
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-10 12:50:05 +0530

    Create Greeting_Message_Using_Name_And_Age.py
    Python program to accept user's name and age as input and display a greeting message using print() and f-string formatting.

---
+++ Greeting_Message_Using_Name_And_Age.py
@@ -0,0 +1,13 @@
+#Program_18
+
+# W.A.Python program to take user's name and age as input and display a greeting message.
+
+name = input("Enter your name:-> ")
+age = int(input("Enter your age:-> "))
+
+print("Hello", name + "!")
+print("You are", age, "years old")
+
+#or
+
+print(f"Hello {name}! You are {age} years old.")
