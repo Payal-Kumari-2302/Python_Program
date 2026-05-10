5cefd16ba6a0fd4986ee40abc1bb5409396da972
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-10 12:58:54 +0530

    Create Kilometer_Unit_Conversion.py
    Python program to convert kilometer into millimeter, centimeter, decimeter, meter, decameter, and hectometer using arithmetic operations.

---
+++ Kilometer_Unit_Conversion.py
@@ -0,0 +1,19 @@
+#Program_25
+
+# W.A.Python program to accept kilometer and convert it into millimeter, centimeter, decimeter, meter, decameter, and hectometer.
+
+km = int(input("Enter kilometer:-> "))
+
+mm = km * 10**6
+cm = km * 10**5
+dm = km * 10**4
+m = km * 10**3
+decam = km * 10**2
+hm = km * 10
+
+print("Millimeter:->", mm)
+print("Centimeter:->", cm)
+print("Decimeter:->", dm)
+print("Meter:->", m)
+print("Decameter:->", decam)
+print("Hectometer:->", hm)
