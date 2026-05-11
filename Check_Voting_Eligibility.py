45fa75d5108d22a53c6857648dd1196e55fa4516
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-11 14:57:50 +0530

    Create Check_Voting_Eligibility.py
    This Python program accepts a person's age and checks whether the person is eligible to vote or not using if-else statements. In the second method, the program also checks whether the person is a citizen before allowing voting eligibility.

---
+++ Check_Voting_Eligibility.py
@@ -0,0 +1,26 @@
+#Program_35
+
+# W.A.Python program to accept any age and display voting eligibility.
+
+#Method_1
+
+age=int(input("Enter your Age:->"))
+
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+
+#Method_2
+
+age=int(input("Enter your Age:->"))
+citizen=input("Are you a citizen? (yes/no):->")
+
+if age>=18:
+    if citizen=="yes":
+        print("Eligible to vote")
+    else:
+        print("Not eligible to vote")
+else:
+    print("Under Age")
