21c8791e7d1f9738e8cd1ab9025d14928520a169
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-11 14:47:26 +0530

    Create Even_Odd_Number.py

---
+++ Even_Odd_Number.py
@@ -0,0 +1,47 @@
+# Program_33
+
+# W.A.Python program to accept any number and display whether it is even or odd.
+
+
+num = int(input("Enter any number:-> "))
+
+if num % 2 == 0:
+    print("Even Number")
+
+else:
+    print("Odd Number")
+
+
+#Method_2
+#Using Ternary Operator
+
+num = int(input("Enter any number:-> "))
+
+print("Even Number" if num % 2 == 0 else "Odd Number")
+
+
+#Method_3
+#Using Bitwise Operator
+
+num = int(input("Enter any number:-> "))
+
+if num & 1:
+    print("Odd Number")
+else:
+    print("Even Number")
+
+
+#method_4
+#Using Function
+
+def check(num):
+
+    if num % 2 == 0:
+        return "Even Number"
+
+    return "Odd Number"
+
+
+num = int(input("Enter any number:-> "))
+
+print(check(num))
