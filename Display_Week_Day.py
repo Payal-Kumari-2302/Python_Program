09b4177a6c7a1410de57272a838cfc6efe8cbb36
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-11 14:56:06 +0530

    Create Display_Week_Day.py
    This Python program accepts a week day number from the user and displays the corresponding week day name using if-elif-else statements. If the entered number is not between 1 and 7, it displays an invalid message.

---
+++ Display_Week_Day.py
@@ -0,0 +1,22 @@
+#Program_34
+
+# W.A.Python program to accept any week day number and display the corresponding week day name
+
+day=int(input("Enter any Week Day Number:->"))
+
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+elif day==4:
+    print("Thursday")
+elif day==5:
+    print("Friday")
+elif day==6:
+    print("Saturday")
+elif day==7:
+    print("Sunday")
+else:
+    print("Invalid Week Day Number")
