274b8cdacc245fe6449e50b6a3468f467a8bf521
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-11 14:42:35 +0530

    Create Greatest_Number_Using_If_Else.py

---
+++ Greatest_Number_Using_If_Else.py
@@ -0,0 +1,51 @@
+# Program_32
+
+# if, elif and else Statement
+# W.A.Python program to accept any two numbers and display the greatest number.
+
+
+# ---------------------------------
+# Using if Statement
+# ---------------------------------
+
+num1 = int(input("Enter 1st number:-> "))
+num2 = int(input("Enter 2nd number:-> "))
+
+if num1 > num2:
+    print("Greatest number:", num1)
+
+if num2 > num1:
+    print("Greatest number:", num2)
+
+if num1 == num2:
+    print("Both numbers are equal")
+
+
+# ---------------------------------
+# Using else Statement
+# ---------------------------------
+
+num1 = int(input("\nEnter 1st number:-> "))
+num2 = int(input("Enter 2nd number:-> "))
+
+if num1 > num2:
+    print("Greatest number:", num1)
+else:
+    print("Greatest number:", num2)
+
+
+# ---------------------------------
+# Using if, elif and else Statement
+# ---------------------------------
+
+num1 = int(input("\nEnter 1st number:-> "))
+num2 = int(input("Enter 2nd number:-> "))
+
+if num1 > num2:
+    print("Greatest number:", num1)
+
+elif num2 > num1:
+    print("Greatest number:", num2)
+
+else:
+    print("Both numbers are equal")
