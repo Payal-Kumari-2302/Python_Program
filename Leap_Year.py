a1fc4312502c81ed6f5e37d6367d004af5e0345a
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-12 07:13:46 +0530

    Create Leap_Year.py
    This Python program accepts a year from the user and checks whether the year is a leap year or not using if and else statements. A year is considered a leap year if it is divisible by 400 or divisible by 4 but not divisible by 100.

---
+++ Leap_Year.py
@@ -0,0 +1,11 @@
+#Program_38
+
+# W.A.Python Program to accept any year and display whether it is a leap year or not.
+
+year=int(input("Enter any year:->"))
+
+if((year%400==0) or (year%4==0 and year%100!=0)):
+    print("Leap Year")
+
+else:
+    print("Not a Leap Year")
