d3af5ca4e08289fcc0083669a250de010d645e2e
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-13 21:44:41 +0530

    Create Kilometer_To_Miles_Converter.py
    A simple Python program to convert distance from kilometers to miles using a conversion factor.

---
+++ Kilometer_To_Miles_Converter.py
@@ -0,0 +1,10 @@
+#W.A.Python program to convert kilometer into miles
+
+km = int(input("Enter distance in kilometer:->"))
+
+# Conversion factor: 1 km = 0.621371 miles
+conversion_factor = 0.621371
+
+miles = km * conversion_factor
+
+print(f"{km} kilometer is equal to {miles} miles")
