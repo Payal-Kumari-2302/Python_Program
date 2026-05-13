8bdb082fe217e728b075dea2556cd3839b797507
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-13 21:32:55 +0530

    Create Positive_Negative_Zero_Number.py
    A simple Python program to check whether a number is Positive, Negative, or Zero using if-elif-else statements.

---
+++ Positive_Negative_Zero_Number.py
@@ -0,0 +1,12 @@
+#Program_36
+
+# W.A.Python program to accept any number and display Positive, Negative and Zero number
+
+num = int(input("Enter any number:->"))
+
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else:
+    print("Zero Number")
