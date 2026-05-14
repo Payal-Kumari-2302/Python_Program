95abb17549d113cbe72db0280b170145cacbab0d
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-14 11:03:16 +0530

    Create fruits_list_indexing.py

---
+++ fruits_list_indexing.py
@@ -0,0 +1,10 @@
+#Program_45
+
+# Create a list of fruits and access elements using indexing
+
+fruits_list = ["Apple", "Banana", "Cherry", "Mango"]
+
+print("First fruit:", fruits_list[0])
+print("Second fruit:", fruits_list[1])
+print("Third fruit:", fruits_list[2])
+print("Fourth fruit:", fruits_list[3])
