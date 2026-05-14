2d3bbe8eb9be8d24affa167efd2f29448756304d
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-14 10:57:35 +0530

    Create days_to_years_weeks_days.py
    Python Program to Convert Given Number of Days into Years, Weeks, and Remaining Days

---
+++ days_to_years_weeks_days.py
@@ -0,0 +1,15 @@
+#Program_42
+
+# W.A.Python program to convert days into years, weeks, and days
+
+days = int(input("Enter number of days:-> "))
+
+years = days // 365
+remaining_days = days % 365
+
+weeks = remaining_days // 7
+days_left = remaining_days % 7
+
+print("Years:", years)
+print("Weeks:", weeks)
+print("Days:", days_left)
