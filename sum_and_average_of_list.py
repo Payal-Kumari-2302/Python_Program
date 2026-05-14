41a9e76fc4cca61ef6844b9f47dc8d67f93d56a3
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-14 11:04:11 +0530

    Create sum_and_average_of_list.py
    This Python program calculates the sum and average of numbers present in a list using built-in functions like sum() and len(). It helps beginners understand basic list operations in Python.

---
+++ sum_and_average_of_list.py
@@ -0,0 +1,11 @@
+#Program_51
+
+# Given a list of numbers, find the sum and average
+
+list = [2, 5, 7, 3, 8, 1]
+
+add = sum(list)
+avg = add / len(list)
+
+print("Sum of list:", add)
+print("Average of list:", avg)
