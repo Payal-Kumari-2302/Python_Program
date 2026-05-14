170539656ae1bb2ac6219c351bc496ec453015ae
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-14 10:56:03 +0530

    Create max_min_list.py
    Python Program to Find Maximum and Minimum Values in a List

---
+++ max_min_list.py
@@ -0,0 +1,11 @@
+#Program_41
+
+# W.A.Python program to find the maximum and minimum values in a list
+
+list = [1, 3, 6, 19, 50]
+
+maximum = max(list)
+minimum = min(list)
+
+print("Maximum Value:", maximum)
+print("Minimum Value:", minimum)
