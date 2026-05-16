a0830c27c68fc88a9ebce7490abdecfb26edd106
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-16 08:50:53 +0530

    Create Time-Zone program to display current time
    This program retrieves and displays the current time, breaking it down into hours, minutes, and seconds.

---
+++ Time_Zone.py
@@ -0,0 +1,22 @@
+#Program_51
+
+
+# Time-Zone Program
+
+import time
+
+# Full current time
+current_time = time.strftime("%H:%M:%S")
+print("Current Time:", current_time)
+
+# Hour
+hour = int(time.strftime("%H"))
+print("Hour:", hour)
+
+# Minute
+minute = int(time.strftime("%M"))
+print("Minute:", minute)
+
+# Second
+second = int(time.strftime("%S"))
+print("Second:", second)
