db319fd85357c04381433c9e93ee83695f94cf79
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-17 14:44:45 +0530

    Create Print_1_to_10_Num.py

---
+++ Print_1_to_10_Num.py
@@ -0,0 +1,18 @@
+# Program_58
+
+# W.A.P to display numbers from 1 to 10 using loops
+
+
+# ---------- FOR LOOP ----------
+
+for i in range(1, 11):
+    print(i)
+
+
+# ---------- WHILE LOOP ----------
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
