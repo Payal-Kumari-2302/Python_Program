22bc448a7ac380b6ace3952608ab904141c486c9
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-17 14:39:40 +0530

    Update Vowel_or_Not_Vowel.py

--- Vowel_or_Not_Vowel.py
+++ Vowel_or_Not_Vowel.py
@@ -62,3 +62,18 @@ match ch:
         print("Vowel")
     case _:
         print("Not a vowel")
+
+
+
+#W.A.P to accept any character and check vowel,semi vowel or not vowel
+
+
+ch=input("Enter any Character:->")
+if(ch=="A" or ch=="a" or ch=="E" or ch=="e" or ch=="I" or ch=="i" or ch=="O" or ch=="o" or ch=="U" or ch=="u"):
+    print("Vowel")
+elif (ch=="W" or ch == "w" or ch== "Y" or ch=="y"):
+    print("Semi vowel")
+elif (ch=="B" or ch=="b" or ch=="C" or ch=="c" or ch== "D" or ch=="d" or ch=="F" or ch=="f" or ch=="G" or ch=="g" or ch=="H" or ch=="h" or ch=="I" or ch=="i" or ch=="J" or ch=="j" or ch=="K" or ch=="k" or ch=="L" or ch=="l" or ch=="M" or ch=="m" or ch=="N" or ch=="O" or ch=="P" or ch=="p" or ch=="Q" or ch=="R" or ch=="r" or ch=="S" or ch=="s" or ch=="T" or ch=="t" or ch=="V" or ch=="v" or ch=="X" or ch=="x" or ch=="Z" or ch=="z"):
+    print("Not Vowel[Consonant]")
+else:
+    print("Not an alphabet")
