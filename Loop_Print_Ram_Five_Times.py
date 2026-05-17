785165f9ab5a0066cd951fe53400b482ee07c27b
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-17 14:41:11 +0530

    Create Loop_Print_Ram_Five_Times.py

---
+++ Loop_Print_Ram_Five_Times.py
@@ -0,0 +1,15 @@
+# W.A.P to print the statement "RAM" 5 times using loops
+
+# ---------------- FOR LOOP ----------------
+
+for i in range(5):
+    print("RAM")
+
+
+# ---------------- WHILE LOOP ----------------
+
+num = 1
+
+while num <= 5:
+    print("RAM")
+    num += 1
