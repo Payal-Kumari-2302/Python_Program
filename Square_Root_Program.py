04283e925544434d38d1f16ea062abb860d6773f
Payal Kumari <jaiswalpayal0403@gmail.com>
2026-05-18 11:43:58 +0530

    Create Square_Root_Program.py

---
+++ Square_Root_Program.py
@@ -0,0 +1,11 @@
+#Program_62
+
+#W.A.P to accept a number and display square root of that number
+
+import math
+
+num = int(input("Enter any number:-> "))
+
+root = math.sqrt(num)
+
+print("Square Root is:", root)
